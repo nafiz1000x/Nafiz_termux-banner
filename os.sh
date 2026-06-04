4eca63d96f6076dcf155698c21821c885203ceb1
Nafiz Sheikh <snafiz372@gmail.com>
2026-06-05 00:48:41 +0600

    Add files via upload

---
+++ os.sh
@@ -0,0 +1,162 @@
+#!/bin/bash
+R='\033[1;31m'
+G='\033[1;32m'
+Y='\033[1;93m'
+B='\033[1;94m'
+C='\033[1;96m'
+W='\033[1;97m'
+RS='\033[0m'
+
+term_width=$(tput cols)
+BOX_WIDTH=$(( term_width > 60 ? 58 : term_width - 2 ))
+margin=$(( (term_width - BOX_WIDTH) / 2 ))
+left_pad=$(printf '%*s' "$margin" "")
+
+draw_line() {
+    printf "${C}${left_pad}%s" "$1"
+    for ((i=0; i<BOX_WIDTH-2; i++)); do printf "═"; done
+    printf "%s${RS}\n" "$2"
+}
+
+print_center() {
+    local text="$1"
+    local color="$2"
+    local len=${#text}
+    local space_len=$(( (BOX_WIDTH - 2 - len) / 2 ))
+    printf "${C}${left_pad}║%*s${color}%s${C}%*s║${RS}\n" $space_len "" "$text" $(( BOX_WIDTH - 2 - len - space_len )) ""
+}
+
+banner() {
+    clear
+    
+    local R="\e[1;31m" 
+    local G="\e[1;32m" 
+    local C="\e[1;36m" 
+    local W="\e[1;37m"
+    local Y="\e[1;33m" 
+    local N="\e[0m"    
+
+    echo -e "${C} ______                              ${R}  ___  ____"
+    echo -e "${C}/_  __/__  _________ ___  __  ___  __${R} / _ \/ __/"
+    echo -e "${C} / / / _ \/ ___/ __ '__ \/ / / / |/_/${R}/ // /\ \  "
+    echo -e "${C}/_/  \___/_/  /_/ /_/ /_/\__,_/_/|_| ${R}\___/___/  "
+    echo -e "                                      "
+    echo -e "${W}      --[ ${G}Termux Optimization Tool ${W}]--       "
+    echo -e ""
+    
+    echo -e "${R} [!]${W} Author  : ${C}Nafiz Sheikh (Nafiz)"
+    echo -e "${R} [!]${W} Version : ${Y}v3.5 (Stable)"
+    echo -e "${R} [!]${W} Youtube : ${W}Nafiz tech & hacking"
+    echo -e "${R} [!]${W} GitHub  : ${W}github.com/nafiz1000x"
+    echo -e ""
+    
+    echo -e "${G} ==============================================${N}"
+    echo -e ""
+}
+
+banner
+
+1line() { apt update && apt upgrade; pkg install zsh git figlet toilet ruby wget curl -y; gem install lolcat; clear; cd ~/Termux-os/.object/ && cp -r 'ANSI Shadow.flf' $PREFIX/share/figlet/ASCII-Shadow.flf; git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh; pkg install toilet figlet exa -y; cd ~/Termux-os/.object; rm -rf ~/.termux/colors.properties; rm -rf /data/data/com.termux/files/usr/etc/motd; cp -r .colors.properties ~/.termux/colors.properties; cp -r .termux.properties ~/.termux.properties; curl -L https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/FiraCode/Regular/FiraCodeNerdFont-Regular.ttf > ~/.termux/font.ttf; clear; cd ~/Termux-os ; bash os.sh; termux-open-url nafiz.me && termux-reload-settings; }
+2line() { rm -rf ~/.zshrc; git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh; cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc; cd ~/Termux-os ; bash os.sh; }
+3line() { pkg install zsh; chsh -s zsh; cd ~/Termux-os ; bash os.sh; }
+4line() { chsh -s bash; cd ~/Termux-os ; bash os.sh; }
+5line() { rm -rf ~/.zshrc; cd ~/Termux-os/.object; bash .2.sh; clear ; cd ~/Termux-os ; bash os.sh; }
+6line() { cd ~/Termux-os/.object; bash .1.sh; clear ; cd ~/Termux-os ; bash os.sh; }
+7line() { cd ~/Termux-os/.object; rm -rf ~/.zshrc; chsh -s zsh; bash .3.sh; clear ; cd ~/Termux-os ; bash os.sh; }
+10line() { rm -rf ~/Termux-os; cd; git clone https://github.com/nafiz1000x/Termux-os; cd ~/Termux-os ; bash os.sh; }
+8line() {
+    echo -e "\n${C}Initialising Security Protocol...${RS}"
+    echo -ne "${Y}Create Access Key: ${RS}"
+    read -s new_pass
+    echo
+    
+    lock_code="#LOCK_START
+clear
+echo -e '\033[1;32m'
+echo '  System check...'
+sleep 0.2
+echo '  Encrypted link established.'
+sleep 0.2
+clear
+attempt=1
+while [ \$attempt -le 3 ]; do
+    echo -e \"\n${C}╔══════════════════════════════════════╗\"
+    echo -e \"║        ${R}SECURE SHELL ACCESS           ${C}║\"
+    echo -e \"╚══════════════════════════════════════╝${RS}\"
+    echo -ne \"${Y} [Attempt \$attempt/3] Enter Key: ${RS}\"
+    read -s pass_input
+    echo
+    if [ \"\$pass_input\" = \"$new_pass\" ]; then
+        echo -e \"${G} ACCESS GRANTED.${RS}\"
+        sleep 1
+        clear
+        break
+    else
+        echo -e \"${R} DENIED.${RS}\"
+        if [ \$attempt -eq 3 ]; then
+            exit
+        fi
+        attempt=\$((attempt + 1))
+    fi
+done
+#LOCK_END"
+
+    add_to_top() {
+        local file=$1
+        if [ -f "$file" ]; then
+            echo "$lock_code" > "$file.tmp"
+            cat "$file" >> "$file.tmp"
+            mv "$file.tmp" "$file"
+        else
+            echo "$lock_code" > "$file"
+        fi
+    }
+
+    add_to_top ~/.bashrc
+    [ -f ~/.zshrc ] && add_to_top ~/.zshrc
+
+    echo -e "${G}Lock Configured at the TOP of files.${RS}"
+    sleep 2
+    menu
+}
+
+9line() {
+    sed -i '/#LOCK_START/,/#LOCK_END/d' ~/.bashrc
+    [ -f ~/.zshrc ] && sed -i '/#LOCK_START/,/#LOCK_END/d' ~/.zshrc
+    echo -e "${R}Security Protocol Deactivated.${RS}"
+    sleep 2
+    menu
+}
+
+menu() {
+    banner
+    printf "\n${left_pad}${C}[${W}01${C}]${G} Necessary Setup"
+    printf "\n${left_pad}${C}[${W}02${C}]${G} Zsh Setup"
+    printf "\n${left_pad}${C}[${W}03${C}]${G} Zsh Shell"
+    printf "\n${left_pad}${C}[${W}04${C}]${G} Bash Shell"
+    printf "\n${left_pad}${C}[${W}05${C}]${Y} Zsh Banner"
+    printf "\n${left_pad}${C}[${W}06${C}]${Y} Zsh Theme"
+    printf "\n${left_pad}${C}[${W}07${C}]${Y} Highlight / AutoSuggest"
+    printf "\n${left_pad}${C}[${W}08${C}]${B} Add Cyber Lock ${R}(Top Security)"
+    printf "\n${left_pad}${C}[${W}09${C}]${R} Remove Lock"
+    printf "\n${left_pad}${C}[${W}10${C}]${W} Update Script"
+    printf "\n${left_pad}${C}[${W}00${C}]${R} Exit Terminal\n\n"
+    
+    echo -ne "${left_pad}${C}Selection: ${RS}"
+    read a
+    case $a in
+        1|01) 1line ;;
+        2|02) 2line ;;
+        3|03) 3line ;;
+        4|04) 4line ;;
+        5|05) 5line ;;
+        6|06) 6line ;;
+        7|07) 7line ;;
+        8|08) 9line ;;   # This points to your Cyber Lock function
+        9|09) 10line ;;  # This points to your Remover function
+        10) 8line ;;     # This points to your Update function
+        0|00) exit ;;
+        *) menu ;;
+    esac
+}
+menu
